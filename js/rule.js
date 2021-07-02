793a68647856aa18042588514a153636e1686eb6
Aaron Fenyes <fenyes@ihes.fr>
2021-07-02 17:47:12 +0200

    Implement custom highlight colors
    Fix a bug the Rule constructor that would skip blank columns. For now,
    the resource dictionaries are hard-coded into the Rule class as static
    members. Later, we could split them off into a modular ResourceKit
    class, like we do in the Java application.

--- js/rule.js
+++ js/rule.js
@@ -24,14 +24,28 @@ class Rule {
   to
   stop
   
+  static stopDict = {
+    '': false,
+    '%': true
+  }
+  
+  static colorDict = {
+    '': 'white-highlight',
+    'p': 'pink-highlight',
+    'o': 'orange-highlight',
+    'y': 'yellow-highlight',
+    'g': 'green-highlight',
+    'b': 'blue-highlight',
+    'v': 'violet-highlight'
+  }
+  
   constructor(code) {
-    var tokens = code.split(/\s+/)
+    var tokens = code.split(/\s/)
     if (tokens.length >= 1) this.from = tokens[0]; else this.from = ""
     if (tokens.length >= 2) this.to = tokens[1]; else this.to = ""
     if (tokens.length >= 3) {
-      if (tokens[2] === '') this.stop = false
-      else if (tokens[2] === '%') this.stop = true
-      /*[EXCEPTION] else throw new InterpreterException(InterpreterException.BAD_STOP, tokens[2]);*/
+      if (tokens[2] in Rule.stopDict) this.stop = Rule.stopDict[tokens[2]]
+      /*[EXCEPTION] else throw new InterpreterException(InterpreterException.BAD_STOP, tokens[2])*/
     } else {
       this.stop = false;
     }
@@ -43,13 +57,12 @@ class Rule {
       sound = resources.getSound("");
     }
     */
-    /*[COLOR]
     if (tokens.length >= 5) {
-      if (resources.containsColor(tokens[4])) this.color = resources.getColor(tokens[4]);
-      else throw new InterpreterException(InterpreterException.BAD_COLOR, tokens[4]);
+      if (tokens[4] in Rule.colorDict) this.color = Rule.colorDict[tokens[4]]
+      /*[EXCEPTION] else throw new InterpreterException(InterpreterException.BAD_COLOR, tokens[4])*/
     } else {
-      this.color = resources.getColor("");
-    }*/
+      this.color = Rule.colorDict['']
+    }
   }
   
   // if this rule can be applied to the machine's working string, apply it and
@@ -63,7 +76,7 @@ class Rule {
     /*[SOUND] if (soundOn && this.sound != null) [PLAY SOUND]*/
     var pre = machine.str.slice(0, index)
     var post = machine.str.slice(index + this.from.length)
-    machine.display.innerHTML = `<span>${pre}<span class="white-highlight">${this.from}</span>${post}</span>`
+    machine.display.innerHTML = `<span>${pre}<span class="${this.color}">${this.from}</span>${post}</span>`
     machine.str = pre + this.to + post
     setTimeout(machine.updateDisplay.bind(machine), machine.halfPeriod)
     return true
